a45582d15cf7afac6e33905ce87b409d1806257d
Sergey Aleks <sergaua@gmail.com>
2025-02-02 15:15:15 +0100

    enhanced cred file

--- credentials.py
+++ credentials.py
@@ -1,2 +1,7 @@
-ChatGPT_TOKEN = ""
-BOT_TOKEN = ""
+from dotenv import load_dotenv # Библиотека для работы с переменными окружения
+import os
+
+load_dotenv() # Загружаем переменные из .env
+
+ChatGPT_TOKEN = os.getenv("CHATGPT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
